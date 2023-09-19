8062c076916ceafcb8a86fda133f8d8285775a6a
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2023-09-19 16:17:22 +0200

    Adds build script + smaller fixes for correct links in PDF

---
+++ doc/ibex/tgg/integrate-framework/md2pdf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ -e 'all.md' ]; then rm all.md; fi
+
+# Concat all mds to one file
+for i in *.md;
+    do echo "<div id=\"$i\"></div>" | cat - $i >>all.md
+done
+
+# Compile the file
+pandoc -f markdown --lua-filter=hashids.lua -o test.pdf all.md
+rm all.md
